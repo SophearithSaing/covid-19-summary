f6d756c520ca05396ffcbae8a8f92cd13478a302
sophearith saing <sophearithsaing123@gmail.com>
2020-05-14 14:43:41 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'covid-info';
+  isLoading = true;
+  data = null;
+  countries = [];
+  showCountries = [];
+
+  pageOfItems: Array<any>;
+  totalCountries = 0;
+  countriesPerPage = 5;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<{ Global: any, Countries: any }>('https://api.covid19api.com/summary').subscribe(data => {
+      this.data = data;
+      this.countries = data.Countries;
+      this.totalCountries = this.countries.length;
+      this.showCountries = this.countries.slice(0, this.countriesPerPage);
+      // console.log(this.data);
+      this.isLoading = false;
+      // console.log(this.countriesPerPage * (this.currentPage - 1),
+      //                                      this.countriesPerPage * (this.currentPage - 1) + this.countriesPerPage);
+
+    });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.currentPage = pageData.pageIndex + 1;
+    this.countriesPerPage = pageData.pageSize;
+    this.showCountries = this.countries.slice(this.countriesPerPage * (this.currentPage - 1),
+                                              this.countriesPerPage * (this.currentPage - 1) + this.countriesPerPage);
+    // console.log(this.countriesPerPage * (this.currentPage - 1), this.countriesPerPage * (this.currentPage - 1) + this.countriesPerPage);
+    // console.log(this.showCountries);
+    // this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -17,6 +21,10 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
     HttpClientModule
   ],
   providers: [],
